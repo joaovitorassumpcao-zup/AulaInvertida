776c1d1abf74073ef2e8fbc9023a91a113f5daba
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-05-31 15:25:57 -0300

    ...

--- src/Exer7.java
+++ src/Exer7.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Exer7 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = new int[5];
 
+        System.out.println("Digite sua lista de 5 valores: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(" -> ");
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.print("Sua lista é: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
