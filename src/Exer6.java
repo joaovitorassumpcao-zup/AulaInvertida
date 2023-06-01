f018349de155452ff5309e03a412e180538ffb1e
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-05-31 23:06:37 -0300

    Exercicio 6

--- src/Exer6.java
+++ src/Exer6.java
@@ -1,2 +1,25 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.Scanner;
+
 public class Exer6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Defina o inicio do intervalo: ");
+        int intervaloInicio = scanner.nextInt();
+
+        System.out.print("Defina o fim do intervalo: ");
+        int intervaloFim = scanner.nextInt();
+
+        if (intervaloInicio >= intervaloFim) {
+            System.out.println("Intervalo de valores inválido!");
+            return;
+        }
+
+        for (int i = intervaloInicio; i <= intervaloFim; i++) {
+                System.out.println(i);
+        }
+
+    }
 }
