e73b2cf90403dc4d09659c78af0206e791885730
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-05-31 23:57:01 -0300

    Exercicio 11

---
+++ src/Exer11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exer11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Peso do peixe: ");
+        int peso = scanner.nextInt();
+
+        if (peso > 50) {
+            int excesso = peso - 50;
+            float multa = excesso * 4;
+
+            System.out.println("Peso: " + peso + " Kg");
+            System.out.println("Excesso: " + excesso + " Kg");
+            System.out.println("Multa (R$4,00/Kg):  R$ " + multa);
+        }
+    }
+}
